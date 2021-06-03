9f5fa647e6b2c51fb78d98d696df7cdea1264b42
puneet150101 <puneetsingh.150101@gmail.com>
2021-06-03 16:21:35 +0530

    Used array list to store data and taking input via buffered reader and storing data in Array integer list

--- hello.java
+++ hello.java
@@ -1,27 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
-class test
-{
-    int a,b;
-    test(int a, int b)
-    {
-        this.a=a;
-        this.b=b;
-        System.out.print("Value a:"+a+"\n"+"Value b:"+b);
+class test {
+    int a, b;
+
+    test(int a, int b) {
+        this.a = a;
+        this.b = b;
+        System.out.print("Value a:" + a + "\n" + "Value b:" + b);
     }
-    public int sum()
-    {
-        return a+b;
+
+    public int sum() {
+        return a + b;
     }
 }
+
 public class hello {
-    public static void main(String[] args)  throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-        test a= new test(2,3);
-        int n = a.sum();
-        System.out.println("\nworked:"+n);
+        ArrayList<Integer> al = new ArrayList();
+        for (String i : rd.readLine().split(" "))
+            al.add(Integer.parseInt(i));
+        for (Integer i : al)
+            System.out.println(i);
     }
 }

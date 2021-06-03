16bc48b0e66bd741736297808012719f7d5e8801
puneet150101 <puneetsingh.150101@gmail.com>
2021-06-03 12:27:00 +0530

    testing taking input with buffered reader

--- hello.java
+++ hello.java
@@ -3,16 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class hello {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException
+    {
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-        // String test=read.readLine();
-        // char[] a={'t','e','s'};
-        // a[2]='q';
-        // int i = Integer.parseInt(rd.readLine());
-        int k = 9;
-        if (k > 10) {
-            throw new ArithmeticException();
-        }
-        System.out.println("worked");
+        int n = Integer.parseInt(rd.readLine());
+        System.out.println("worked:"+n);
     }
 }

b1f09a8a05349e51b8fc720b13aca15ffc877cbd
puneet150101 <puneetsingh.150101@gmail.com>
2021-06-03 12:57:30 +0530

    Trying class vars and methods

--- hello.java
+++ hello.java
@@ -2,11 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+class test
+{
+    int a,b;
+    test(int a, int b)
+    {
+        this.a=a;
+        this.b=b;
+        System.out.print("Value a:"+a+"\n"+"Value b:"+b);
+    }
+    public int sum()
+    {
+        return a+b;
+    }
+}
 public class hello {
     public static void main(String[] args)  throws Exception
     {
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(rd.readLine());
-        System.out.println("worked:"+n);
+        test a= new test(2,3);
+        int n = a.sum();
+        System.out.println("\nworked:"+n);
     }
 }

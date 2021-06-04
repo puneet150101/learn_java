0e597ac741abc1a028a5dbee005da580c14be276
puneet150101 <puneetsingh.150101@gmail.com>
2021-06-04 14:34:44 +0530

    Using comparator method and interface to do a custom sort with exception handling and constructor overloading

--- hello.java
+++ hello.java
@@ -1,8 +1,9 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class test implements Comparable<test> {
+class test  {
     int age;
     String name;
 
@@ -10,18 +11,29 @@ class test implements Comparable<test> {
         this.name = name;
         this.age = age;
     }
-
-    public int compareTo(test a) {
-        return this.name.compareTo(a.name);
+    test(String name)
+    {
+        this.name=name;
+        this.age=25;
     }
-
+    test()
+    {
+        this.name=new String("default");
+        this.age=25;
+    }
     public String toString() {
         return "Name:" + this.name + " Age:" + this.age;
     }
 }
-
+class sortbyname implements Comparator<test>
+{
+    public int compare(test a,test b)
+    {
+        return a.name.compareTo(b.name);
+    }
+}
 public class hello {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter the number of entries:");
         int n = Integer.parseInt(rd.readLine());
@@ -29,9 +41,23 @@ public class hello {
         while (n-- > 0) {
             System.out.print("Enter name and age:");
             String[] tmp = rd.readLine().split(" ");
-            data.add(new test(tmp[0], Integer.parseInt(tmp[1])));
+            try{
+                data.add(new test(tmp[0], Integer.parseInt(tmp[1])));
+            }
+            catch (ArrayIndexOutOfBoundsException e)
+            {
+                if(tmp[0].length()>0)
+                {
+                    
+                    data.add(new test(tmp[0]));
+                }
+                else 
+                {
+                    data.add(new test());
+                }
+            }
         }
-        Collections.sort(data);
+        Collections.sort(data,new sortbyname());
         for (test i : data)
             System.out.println(i.toString());
     }
